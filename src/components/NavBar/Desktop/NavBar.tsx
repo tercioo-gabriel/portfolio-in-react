e8a37b29695499882e979f4363a4760d8c1099b9
Tercio Gabriel <terciogabrieldejesus@gmail.com>
2023-10-31 12:05:15 -0300

    finalizando site

--- src/components/NavBar/Desktop/NavBar.tsx
+++ src/components/NavBar/Desktop/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
-import Image from 'next/image';
 
 const NavBar = () => {
   const [largura, setLargura] = useState(0);
